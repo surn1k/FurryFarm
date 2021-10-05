5dca1951b5559642591b1f6f61ae393927f7190b
elestrias <rus8-2002@mail.ru>
2021-10-06 00:17:16 +0300

    Singelton + Table Abstractclass
    Signed-off-by: elestrias <rus8-2002@mail.ru>

---
+++ src/com/company/Farmers.java
@@ -0,0 +1,47 @@
+package com.company;
+import java.sql.ResultSet;
+import java.sql.SQLData;
+import java.util.ArrayList;
+
+
+
+public class Farmers extends Table{
+    public Farmers(){
+        tableName = "FARMERS";
+    }
+
+    @Override
+    String getName() {
+        return tableName;
+    }
+
+    @Override
+    String getRef(int id) {
+        String request = "SELECT " + id + "FROM COMPANY";
+        try {
+            ResultSet result = DataBase.GetDataBase().AskDB(request);
+        } catch (Exception ignored){}
+    }
+
+    @Override
+    public void writeRef(int id) {
+
+    }
+
+    @Override
+    public void deleteRef(int id) {
+
+    }
+
+    @Override
+    public int getTableSize() {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<String> getAll() {
+        return null;
+    }
+
+    private String tableName;
+}
