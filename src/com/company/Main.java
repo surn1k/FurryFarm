051d040fa6af1dcc9150085a9a85bcd40da290be
elestrias <rus8-2002@mail.ru>
2021-10-06 00:17:16 +0300

    Singelton + Table Abstractclass
    Signed-off-by: elestrias <rus8-2002@mail.ru>

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Main {
+    Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
