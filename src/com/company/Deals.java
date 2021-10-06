b26526c77e94a37000f2099cc413c5135c516305
elestrias <rus8-2002@mail.ru>
2021-10-06 22:03:52 +0300

    find ref for Table 1 done
    Signed-off-by: elestrias <rus8-2002@mail.ru>

---
+++ src/com/company/Deals.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Deals extends Table{
+
+    @Override
+    String getName() {
+        return null;
+    }
+
+    @Override
+    ArrayList<String> getRef(int id) {
+        return null;
+    }
+
+    public void writeRef(int id, int dealer_id, String info){
+        String request =
+    }
+
+    @Override
+    public void deleteRef(int id) {
+
+    }
+
+    @Override
+    public int getTableSize() {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<String> getAll() {
+        return null;
+    }
+}
